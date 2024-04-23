8457c503264377004e5c8b57347710e9dddf0732
David Duarte <37241920+daviddev16@users.noreply.github.com>
2024-04-22 23:17:52 -0300

    :memo: Added DependencyManager java documentation.
    :art: Improved structure of 'installType' and 'installInstance'.
    
    :art: Added InstallationType to support standalone/dynamic dependency injection and instantiation.
    
    :art: Added dynamic parameter to @Managed to support better 'installType' operations.

--- src/main/java/io/github/dependency4j/exception/InstallationFailedException.java
+++ src/main/java/io/github/dependency4j/exception/InstallationFailedException.java
@@ -6,7 +6,6 @@ package io.github.dependency4j.exception;
 public class InstallationFailedException extends RuntimeException {
 
     public InstallationFailedException(String packageName, Throwable cause) {
-        super("Installation failed for package \"%s\".".formatted(packageName), cause);
+        super("Package \"%s\" installation failed.".formatted(packageName), cause);
     }
-
 }
