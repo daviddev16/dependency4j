dc329b9c78c5bbd467d1aa23d5baf6f9424d6782
David Duarte <37241920+daviddev16@users.noreply.github.com>
2024-04-29 15:06:28 -0300

    :fix: Fixed ClassFinder when using Kotlin (#10)

--- src/main/java/io/github/dependency4j/exception/ScanFailedException.java
+++ src/main/java/io/github/dependency4j/exception/ScanFailedException.java
@@ -1,16 +1,12 @@
 package io.github.dependency4j.exception;
 
-import static java.lang.String.*;
-
 public final class ScanFailedException extends RuntimeException {
-
-    public ScanFailedException(String packageName, ClassLoader classLoader, Throwable cause) {
-        super(format("Occurred an error while scanning the package \"%s\" with \"%s\" class loader.",
-                packageName, classLoader.getName()), cause);
-    }
 
     public ScanFailedException(String message) {
         super(message);
     }
 
+    public ScanFailedException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
